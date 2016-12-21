1656bc47edcea3ce64dacd77b7a3cdf497dd91c3
David Klingenberg <dklingenberg@gmail.com>
2016-12-22 00:49:25 +0100

    waltz

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,44 @@ import * as d3 from 'd3';
 import {scaleLinear} from 'd3-scale';
 import {Leg} from './steps';
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 let svg = d3.select('svg');
 let nodes = [
 	{
-		x: 100,
-		y: 100,
-		rotation: 0,
+		x: 750,
+		y: 500,
+		rotation: 45,
 		leg: Leg.LF
 	},
 	{
-		x: 130,
-		y: 100,
-		rotation: 0,
+		x: 780,
+		y: 500,
+		rotation: 45,
 		leg: Leg.RF
 	}
 ];
+
+function dragStarted(d) {
+  d3.select(this).raise().classed("active", true);
+}
+
+function dragged(d) {
+	let x = d3.event.x;
+	let y = d3.event.y;
+
+	d3.select(this)
+		.attr("transform", `translate(${x}, ${y})`);
+}
+
+function dragEnded(d) {
+	console.log(d);
+	console.log(d3.event);
+  d3.select(this).classed("active", false);
+}
+
 
 svg.append('rect')
 	.attr('width', 1000)
@@ -32,8 +55,14 @@ selection.enter()
 	.append('g')
 	.attr('class', 'node')
 	.attr('transform', (d) => {
-		return `translate(${d.x}, ${d.y})`;
+		let rotation = d.leg === Leg.LF ? d.rotation : d.rotation + 10;
+		return `translate(${d.x}, ${d.y}) rotate(${rotation} 12.37 35)`;
 	})
+	.call(d3.drag()
+		.on('start', dragStarted)
+		.on('drag', dragged)
+		.on('end', dragEnded)
+	)
 	.append('image')
 	.attr('xlink:href', (d: any): string => {
 		if (d.leg === Leg.LF) {
@@ -84,3 +113,66 @@ function rotate(rotation: number) {
 			return `translate(${d.x}, ${d.y}) rotate(${d.rotation})`;
 		});
 }
+
+async function waltz() {
+
+	let steps = [
+		{
+			leg: Leg.RF,
+			x: 50,
+			y: -50,
+			rotation: 0
+		},
+		{
+			leg: Leg.LF,
+			x: 150,
+			y: -75,
+			rotation: 90
+		},
+		{
+			leg: Leg.RF,
+			x: 25,
+			y: -20,
+			rotation: 135
+		}
+	];
+
+	let i = 0;
+
+	setInterval(() => {
+		if (i < steps.length) {
+			let step = steps[i];
+
+			if (step.leg === Leg.LF) {
+				nodes[0].x += step.x;
+				nodes[0].y += step.y;
+				nodes[0].rotation += step.rotation;
+			} else {
+				nodes[1].x += step.x;
+				nodes[1].y += step.y;
+				nodes[1].rotation += step.rotation;
+			}
+
+			let selection = svg.selectAll('g.node')
+				.data(nodes);
+
+			let transition = d3.transition('move')
+				.duration(1000);
+
+			selection.transition(transition)
+				.attr('transform', (d: any) => {
+					console.log(d);
+					return `translate(${d.x}, ${d.y}) rotate(${d.rotation})`;
+				});
+		}
+
+		i++;
+	}, 1000);
+
+}
+
+d3.select('#waltz')
+	.on('click', () => {
+		waltz();
+		console.log('Button click');
+	});
