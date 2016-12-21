38ef9df3e31c683e9388569d6827420c70fa3068
David Klingenberg <dklingenberg@gmail.com>
2016-12-21 23:04:11 +0100

    Update movement

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,49 @@ import * as d3 from 'd3';
 import {scaleLinear} from 'd3-scale';
 
 let svg = d3.select('svg');
-let nodes = [ {x: 10, y: 10} ];
+let nodes = [ {x: 100, y: 100} ];
 
 svg.append('rect')
 	.attr('width', 1000)
 	.attr('height', 1000)
-	.attr('fill', 'transparent');
+	.attr('fill', 'transparent')
+	.attr('stroke', 'black')
+	.attr('stroke-opacity', 2);
 
 let node = svg.selectAll('g.node')
 	.data(nodes)
 	.enter()
 	.append('g')
-	.attr('transform', function(d) { return 'translate(' + d.y + ',' + d.x + ')'; } )
+	.attr('transform', function(d) { return 'translate(' + d.y + ',' + d.x + ')'; } );
 
 node.append('image')
 	.attr('xlink:href', './assets/man-shoe.svg')
-	.attr('width', 100)
-	.attr('height', 100);
+	.attr('width', 30)
+	.attr('height', 30);
 
 svg.append('line');
 
+
+d3.select('#start')
+	.on('click', () => {
+		move(100, 0);
+		console.log('Button click');
+	});
+
+
+function move(x: number, y: number) {
+		nodes[0].x += x;
+		nodes[0].y += y;
+
+		let selection = svg.selectAll('g')
+			.data(nodes);
+
+		let transition = d3.transition('move')
+			.duration(1000);
+
+		selection.transition(transition)
+			.attr('transform', (d: any) => {
+				console.log(d);
+				return `translate(${d.x}, ${d.y})`;
+			});
+}
