e575a107e7e9fb6d667f1ad0e89c772ec0b582e6
David Klingenberg <dklingenberg@gmail.com>
2016-12-21 23:43:08 +0100

    Working on movement

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import * as d3 from 'd3';
 import {scaleLinear} from 'd3-scale';
+import {Leg} from './steps';
 
 let svg = d3.select('svg');
-let nodes = [ {x: 100, y: 100} ];
+let nodes = [
+	{
+		x: 100,
+		y: 100,
+		rotation: 0,
+		leg: Leg.LF
+	},
+	{
+		x: 130,
+		y: 100,
+		rotation: 0,
+		leg: Leg.RF
+	}
+];
 
 svg.append('rect')
 	.attr('width', 1000)
@@ -11,23 +25,29 @@ svg.append('rect')
 	.attr('stroke', 'black')
 	.attr('stroke-opacity', 2);
 
-let node = svg.selectAll('g.node')
-	.data(nodes)
-	.enter()
+let selection = svg.selectAll('g.node')
+	.data(nodes);
+
+selection.enter()
 	.append('g')
-	.attr('transform', function(d) { return 'translate(' + d.y + ',' + d.x + ')'; } );
-
-node.append('image')
-	.attr('xlink:href', './assets/man-shoe.svg')
-	.attr('width', 30)
-	.attr('height', 30);
-
-svg.append('line');
-
+	.attr('class', 'node')
+	.attr('transform', (d) => {
+		return `translate(${d.x}, ${d.y})`;
+	})
+	.append('image')
+	.attr('xlink:href', (d: any): string => {
+		if (d.leg === Leg.LF) {
+			return './assets/man-shoe-left.svg';
+		} else {
+			return './assets/man-shoe-right.svg';
+		}
+	})
+	.attr('height', 35);
 
 d3.select('#start')
 	.on('click', () => {
 		move(100, 0);
+		//rotate(45);
 		console.log('Button click');
 	});
 
@@ -36,7 +56,7 @@ function move(x: number, y: number) {
 		nodes[0].x += x;
 		nodes[0].y += y;
 
-		let selection = svg.selectAll('g')
+		let selection = svg.selectAll('g.node')
 			.data(nodes);
 
 		let transition = d3.transition('move')
@@ -47,4 +67,20 @@ function move(x: number, y: number) {
 				console.log(d);
 				return `translate(${d.x}, ${d.y})`;
 			});
+}
+
+function rotate(rotation: number) {
+	nodes[0].rotation += rotation;
+
+	let selection = svg.selectAll('g.node')
+		.data(nodes);
+
+	let transition = d3.transition('rotate')
+		.duration(1000);
+
+	selection.transition(transition)
+		.attr('transform', (d: any) => {
+			console.log(d);
+			return `translate(${d.x}, ${d.y}) rotate(${d.rotation})`;
+		});
 }
